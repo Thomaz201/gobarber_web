a096a51e554366aa84115b8ebc49d9aab5340419
Thomaz201 <lucasthomazr@hotmail.com>
2020-06-28 03:38:23 -0300

    SignIn created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+import SignIn from './pages/Signin/index';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
