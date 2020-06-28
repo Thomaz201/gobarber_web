ee69a353f3d08b9b09413209f309a990e1e8ad17
Thomaz201 <lucasthomazr@hotmail.com>
2020-06-28 18:00:47 -0300

    Registry Page done

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import BackgroundImg from '../../assets/sign-in-background.png';
+import BackgroundImg from '../../assets/sign-up-background.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -43,7 +43,7 @@ export const Content = styled.div`
   }
 
   > a {
-    color: #ff9000;
+    color: #f4ede8;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -53,7 +53,7 @@ export const Content = styled.div`
     align-items: center;
 
     &:hover {
-      color: ${shade(0.2, '#FF9000')};
+      color: ${shade(0.2, '#f4ede8')};
     }
 
     svg {
